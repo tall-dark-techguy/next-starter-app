0f1857f8cf5e4f6a0f87d068a5ce2f68fa198706
Demojevu <demojevu@gmail.com>
2020-09-26 21:13:40 -1100

    Refactor layout page and create sideBar component

---
+++ components/sidebar.jsx
@@ -0,0 +1,60 @@
+import { Sidebar, Menu } from "semantic-ui-react";
+import Link from "next/link";
+
+function SideBar({ visible, setVisible, isAuthenticated }) {
+  return (
+    <Sidebar
+      as={Menu}
+      // animation: slide out, slide along, push
+      direction="left"
+      animation="push"
+      className="black"
+      icon="labeled"
+      inverted
+      onHide={() => setVisible(false)}
+      vertical
+      visible={visible}
+      width="thin"
+    >
+      {!isAuthenticated() ? (
+        <>
+          <Link href="/">
+            <a className="item" onClick={() => setVisible(false)}>
+              <h1 style={{ color: "orange" }}>
+                <i className="react icon"></i>
+              </h1>
+            </a>
+          </Link>
+
+          <Link href="/">
+            <a className="item" onClick={() => setVisible(false)}>
+              <i className="home icon"></i>
+              <p>Home</p>
+            </a>
+          </Link>
+
+          <Link href="/login">
+            <a className="item">
+              <i className="sign in icon"></i>
+              <p>Login</p>
+            </a>
+          </Link>
+
+          <Link href="/signup">
+            <a className="item">
+              <i className="user icon"></i> <p>Sign up</p>
+            </a>
+          </Link>
+        </>
+      ) : (
+        <Link href="/user">
+          <a className="item">
+            <i className="user icon"></i> <p>User</p>
+          </a>
+        </Link>
+      )}
+    </Sidebar>
+  );
+}
+
+export default SideBar;
--- layouts/index.jsx
+++ layouts/index.jsx
@@ -1,9 +1,9 @@
-import Link from "next/link";
 import Head from "../components/head";
 import Nav from "../components/nav";
-import { Menu, Sidebar } from "semantic-ui-react";
+import { Sidebar } from "semantic-ui-react";
 import { useState } from "react";
 import { isAuthenticated } from "../utils/authService";
+import SideBar from "../components/sidebar";
 import FixedBottomFooter from "../components/fixedBottomFooter";
 
 function Layout({ children, title = "Next Starter App By Desse" }) {
@@ -19,48 +19,11 @@ function Layout({ children, title = "Next Starter App By Desse" }) {
           height: "100vh",
         }}
       >
-        <Sidebar
-          as={Menu}
-          // animation: slide out, slide along, push
-          direction="left"
-          animation="push"
-          className="black"
-          icon="labeled"
-          inverted
-          onHide={() => setVisible(false)}
-          vertical
+        <SideBar
           visible={visible}
-          width="thin"
-        >
-          {!isAuthenticated() ? (
-            <>
-              <div className="item">
-                <h1 style={{ color: "orange" }}>
-                  <i className="react icon"></i>
-                </h1>
-              </div>
-
-              <Link href="/login">
-                <a className="item">
-                  <i className="sign in icon"></i>
-                  <p>Login</p>
-                </a>
-              </Link>
-
-              <Link href="/signup">
-                <a className="item">
-                  <i className="user icon"></i> <p>Sign up</p>
-                </a>
-              </Link>
-            </>
-          ) : (
-            <Link href="/user">
-              <a className="item">
-                <i className="user icon"></i> <p>User</p>
-              </a>
-            </Link>
-          )}
-        </Sidebar>
+          setVisible={setVisible}
+          isAuthenticated={isAuthenticated}
+        />
 
         {/* Site and page content goes here :=() */}
         <Sidebar.Pusher>
